d7d782be696deb6129fcaa55449ad481a13d0bb1
P-iotic <jago14.rose@gmail.com>
2025-05-19 21:35:09 +0200

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const readMoreBtn = document.getElementById('read-more');
-  const storyHidden = document.getElementById('story-hidden');
-
-  readMoreBtn.addEventListener('click', () => {
-    storyHidden.classList.toggle('show');
-    readMoreBtn.textContent = storyHidden.classList.contains('show') ? 'Read Less' : 'Read More';
-  });
-});
